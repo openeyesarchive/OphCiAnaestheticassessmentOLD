edd9beec1dc044780e69dd2e9150fbd70c80c63b
Mark <m@rkw.me>
2014-01-14 09:53:11 +0000

    bugfix

--- migrations/m130403_064932_fix_asa_element_name.php
+++ migrations/m130403_064932_fix_asa_element_name.php
@@ -4,7 +4,7 @@ class m130403_064932_fix_asa_element_name extends CDbMigration
 {
 	public function up()
 	{
-		$event_type = $this->dbConnection->createCommand()->select("*")->from("event_type")->where("class_name = :class_name",array("OphCiAnaestheticassessment"))->queryRow();
+		$event_type = $this->dbConnection->createCommand()->select("*")->from("event_type")->where("class_name = :class_name",array(":class_name" => "OphCiAnaestheticassessment"))->queryRow();
 		$this->update('element_type',array('name'=>'ASA'),"event_type_id = {$event_type['id']} and class_name = 'Element_OphCiAnaestheticassessment_Asa'");
 	}
 
